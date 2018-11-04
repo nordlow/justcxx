bd9dd43ec20bcf5ca117cbbdf51ab47d3a8fb25c
Per Nordlöw <per.nordlow@gmail.com>
2018-11-04 21:44:39 +0100

    *** empty log message ***

--- retain_ptr_test.cpp
+++ retain_ptr_test.cpp
@@ -8,15 +8,22 @@ public:
 #include "sg14/memory.hpp"
 using RP = sg14::retain_ptr<S>;
 
-RP f()
-{
-    return RP(nullptr);
-}
+// RP f()
+// {
+//     return RP(nullptr);
+// }
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
 
 int main(__attribute__((unused)) int argc,
          __attribute__((unused)) const char * argv[],
          __attribute__((unused)) const char * envp[])
 {
-
+    cout << sizeof(S) << endl;
+    cout << sizeof(RP) << endl;
     return 0;
 }
