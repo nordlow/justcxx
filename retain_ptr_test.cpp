5f50f6c7bd957189ae4567eba45d97d8d9807547
Per Nordlöw <per.nordlow@gmail.com>
2018-11-04 22:20:08 +0100

    *** empty log message ***

--- retain_ptr_test.cpp
+++ retain_ptr_test.cpp
@@ -1,29 +1,36 @@
-class S
-{
-    S(int x_) : x(x_) {}
-public:
-    int x;
-};
-
-#include "sg14/memory.hpp"
-using RP = sg14::retain_ptr<S>;
-
-// RP f()
-// {
-//     return RP(nullptr);
-// }
-
 #include <iostream>
 #include <string>
 
 using std::cout;
 using std::endl;
 
+class S
+{
+public:
+    S(int x_) : x(x_) {}
+    ~S()
+    {
+        cout << "dtor" << endl;
+    }
+public:
+    int x;
+    mutable unsigned int _refcount = 1;
+};
+
+struct S_traits final {
+    static void increment (S* x) noexcept { x->_refcount += 1; }
+    static void decrement (S* x) noexcept { x->_refcount -= 1; }
+};
+
+#include "sg14/memory.hpp"
+using RP = sg14::retain_ptr<S, S_traits>;
+
 int main(__attribute__((unused)) int argc,
          __attribute__((unused)) const char * argv[],
          __attribute__((unused)) const char * envp[])
 {
-    cout << sizeof(S) << endl;
-    cout << sizeof(RP) << endl;
+    cout << "sizeof(S): " << sizeof(S) << endl;
+    cout << "sizeof(RP): " << sizeof(RP) << endl;
+    auto rp = RP(new S(42));
     return 0;
 }
