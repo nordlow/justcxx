102e983cac0bfee49aa1508c6299a85efaa3ec6e
Per Nordlöw <per.nordlow@gmail.com>
2018-11-15 22:33:42 +0100

    *** empty log message ***

---
+++ sort_subrange.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#import <algorithm>
+
+/** Sort sub-range [sub_begin, sub_end] of [begin, end].
+ *
+ * Describe at https://www.youtube.com/watch?v=0WlJEz2wb8Y&t=2686s
+ */
+template <typename I>           // I models RandomAccessIterator
+void sort_subrange(I begin, I end,
+                   I sub_begin, I sub_end)
+{
+    if (sub_begin == sub_end) { return; }
+    if (sub_begin != begin)
+    {
+        nth_element(begin, sub_begin, end);
+        ++sub_begin;
+    }
+    partial_sort(sub_begin, sub_begin, end);
+}
