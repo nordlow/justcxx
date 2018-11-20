5f2b87223d8395e5d7717b777583d8e27b04dd38
Per Nordlöw <per.nordlow@gmail.com>
2018-11-20 22:21:54 +0100

    Fix

--- t_rot.cpp
+++ t_rot.cpp
@@ -13,12 +13,14 @@ int main(__attribute__((unused)) int argc,
          __attribute__((unused)) const char * argv[],
          __attribute__((unused)) const char * envp[])
 {
-    uint64_t x = 1;
+    const uint64_t x = 1;
     const auto n = 64;
+
     assert(rotl64(x, 64) == 1);
     for (size_t i = 0; i != n; ++i)
     {
-        cout << rotl64(x, i) << endl;
+        assert(rotr64(rotl64(x, i), i) == 1);
     }
+
     return 0;
 }
