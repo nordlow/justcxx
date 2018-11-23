6b6af7e22737c2b4c424799b117b4a4f49250c83
Per Nordlöw <per.nordlow@gmail.com>
2018-11-23 16:27:46 +0100

    *** empty log message ***

--- t_shared_ptr_keys.cpp
+++ t_shared_ptr_keys.cpp
@@ -1,34 +1,52 @@
 #include <memory>
-#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+#include <cassert>
 
-using Key = int;
-using SharedKey = std::shared_ptr<const Key>;
+using namespace std;
 
-inline bool operator == (const SharedKey &a,
-                         const Key *b)
+using T = size_t;
+using SharedT = std::shared_ptr<const T>;
+
+SharedT makeT(int x)
+{
+    return std::make_shared<T>(x);
+}
+
+inline bool operator == (const SharedT &a,
+                         const T *b)
 {
     return (a.get() == b);
 }
 
-inline bool operator == (const Key *a,
-                         const SharedKey &b)
+inline bool operator == (const T *a,
+                         const SharedT &b)
 {
     return (a == b.get());
 }
 
 int main(int argc, const char *argv[], const char *envp[])
 {
-    std::unordered_map<SharedKey, double> up;
+    T value = 42;
+    const auto x = makeT(value);
+    const auto first = (reinterpret_cast<const size_t *const*>(&x))[0];
+    const auto second = (reinterpret_cast<const size_t *const*>(&x))[1];
+    assert(*first == value);    // value first
+    cout << *second << endl;
 
-    SharedKey si = nullptr;
-    const Key *ip = nullptr;
+    std::unordered_set<SharedT> up;
+
+    SharedT si = nullptr;
+    const T *ip = nullptr;
 
     const bool ok1 = (si == ip);
     const bool ok2 = (ip == si);
 
     auto it1 = up.find(si);
 
-    auto it2 = up.find(ip);     // TODO make compile
+    size_t faked_sharedT[sizeof(SharedT) / 8] = {0, 0};
+
+    auto it2 = up.find(*reinterpret_cast<const SharedT*>(&faked_sharedT));     // TODO make compile
 
     return 0;
 }
