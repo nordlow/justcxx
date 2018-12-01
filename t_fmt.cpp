e987632f2d7c7d49da26aaec3333805683ce79cc
Per Nordlöw <per.nordlow@gmail.com>
2018-12-01 11:21:26 +0100

    Fix

--- t_fmt.cpp
+++ t_fmt.cpp
@@ -4,12 +4,14 @@
 
 #include <string>
 #include <vector>
+#include <experimental/vector>
 
 // http://fmtlib.net
 #include "fmt/format.h"
 #pragma comment(lib, "fmt") // https://stackoverflow.com/questions/12821391/c-visual-studio-linking-using-pragma-comment
 
 using namespace std;
+using namespace std::experimental;
 
 int main(__attribute__((unused)) int argc,
          __attribute__((unused)) const char * argv[],
@@ -19,9 +21,20 @@ int main(__attribute__((unused)) int argc,
     // fmt::print(stderr, "System error code = {}\n", errno);
 
     vector<int> v = {11, 12, 13};
+
     for (const auto &e : v)
     {
         fmt::print(stdout, "{},", e);
     }
+    fmt::print("\n");
+
+    erase_if(v, [](int x){return x == 12;});
+
+    for (const auto &e : v)
+    {
+        fmt::print(stdout, "{},", e);
+    }
+    fmt::print("\n");
+
     return 0;
 }
