c5604802ee0e3e9ec434b24ac4fc09f2e894810c
Per Nordlöw <per.nordlow@gmail.com>
2018-12-01 11:15:15 +0100

    Add stuff

--- t_fmt.cpp
+++ t_fmt.cpp
@@ -2,9 +2,12 @@
  * \brief
  */
 
-#include <iostream>
 #include <string>
 #include <vector>
+
+// http://fmtlib.net
+#include "fmt/format.h"
+#pragma comment(lib, "fmt") // https://stackoverflow.com/questions/12821391/c-visual-studio-linking-using-pragma-comment
 
 using namespace std;
 
@@ -12,10 +15,13 @@ int main(__attribute__((unused)) int argc,
          __attribute__((unused)) const char * argv[],
          __attribute__((unused)) const char * envp[])
 {
+    // fmt::format("The answer is {}.", 42);
+    fmt::print(stderr, "System error code = {}\n", errno);
+
     vector<int> v = {11, 12, 13};
     for (const auto &e : v)
     {
-        cout << e << endl;
+        fmt::print(stdout, "{},", e);
     }
     return 0;
 }
