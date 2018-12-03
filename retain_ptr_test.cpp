5055253d8c3466636e2edfc175f6d20c5f71bd7e
Per Nordlöw <per.nordlow@gmail.com>
2018-12-03 14:59:40 +0100

    *** empty log message ***

--- retain_ptr_test.cpp
+++ retain_ptr_test.cpp
@@ -1,47 +1,58 @@
 #include <iostream>
 
-using std::cout;
-using std::endl;
+using namespace std;
 
-class S {
+class Base
+{
 public:
-  S(int x_) : x(x_) {
-      cout << __PRETTY_FUNCTION__ << ":" << endl;
-  }
-  ~S()
-  {
-      cout << __PRETTY_FUNCTION__ << ":" << endl;
-  }
+    Base(int z) : _z(z)
+    {
+    }
+    int _z = 1;
+};
+
+class Sub : public Base
+{
 public:
-  int x;
-  mutable unsigned int _rc = 1;
+    Sub(int x_)
+        : Base(42), x(x_)
+    {
+        cout << __PRETTY_FUNCTION__ << ":" << endl;
+    }
+    ~Sub() { cout << __PRETTY_FUNCTION__ << ":" << endl; }
+public:
+    int x;
+    mutable unsigned int _rc = 1;
 };
 
 struct S_traits final
 {
-    static void increment (S* x) noexcept
+    static void increment(Sub* x) noexcept
     {
         x->_rc += 1;
         cout << __PRETTY_FUNCTION__ << ": rc=" << x->_rc << endl;
     }
-    static void decrement (S* x) noexcept
+    static void decrement(Sub* x) noexcept
     {
         x->_rc -= 1;
         cout << __PRETTY_FUNCTION__ << ": rc=" << x->_rc << endl;
-        if (x->_rc == 0) { delete x; }
+        if (x->_rc == 0)
+        {
+            delete x;
+        }
     }
 };
 
 #include "sg14/memory.hpp"
-using SP = sg14::retain_ptr<S, S_traits>;
+using RP = sg14::retain_ptr<Sub, S_traits>;
 
 int main(__attribute__((unused)) int argc,
-         __attribute__((unused)) const char * argv[],
-         __attribute__((unused)) const char * envp[])
+         __attribute__((unused)) const char* argv[],
+         __attribute__((unused)) const char* envp[])
 {
-    cout << "sizeof(S): " << sizeof(S) << endl;
-    cout << "sizeof(SP): " << sizeof(SP) << endl;
-    auto x = SP(new S(42));
+    cout << "sizeof(Sub): " << sizeof(Sub) << endl;
+    cout << "sizeof(RP): " << sizeof(RP) << endl;
+    auto x = RP(new Sub(42));
     auto y = x;
     return 0;
 }
